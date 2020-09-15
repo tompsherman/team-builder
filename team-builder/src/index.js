e607cc30f193dd4846459328d7dc61db61d99e13
Tom Sherman <tompsherman@gmail.com>
2020-09-15 16:35:12 -0400

    simple form

--- team-builder/src/index.js
+++ team-builder/src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
+import cuid from "cuid"
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const teamList = [
+  {name: "tom", email: "tom@tom.com", role: "frontend engineer"}
+]
+
+const initialFormValues = {
+  name: "",
+  email: "",
+  role: "",
+}
+
+function SimpleForm(){
+  const [team, setTeam] = useState(teamList)
+  const [formValues, setFormValues] = useState(initialFormValues)
+   
+  const onChange = event => {
+    const {name, value} = event.target
+    setFormValues({...formValues, [name]: value})
+  }
+
+  const onSubmit = event => {
+    const newTeamMember = {
+      name: formValues.name.trim(), email: formValues.email.trim(), role: formValues.role
+    }
+    setTeam(team.concat(newTeamMember))
+    setFormValues(initialFormValues)
+    event.preventDefault()
+  }
+
+  return  <div className="container">
+    <h1>Simple Form</h1>
+    {
+      team.map((team)=>{
+        return (
+          <div key={cuid()}>
+            {team.name} is a {team.role}. Contact info: {team.email}
+          </div>
+        )
+      })
+    }
+    <form onSubmit={onSubmit}>
+      <input name="name" type="text" value={formValues.name} onChange={onChange} placeholder="name"/>
+      <input name="email" type="email" value={formValues.email} onChange={onChange}placeholder="email"/>
+      <select name="role" value={formValues.role} onChange={onChange}>
+        <option>---select option---</option>
+        <option>UI designer</option>
+        <option>frontend engineer</option>
+        <option>backend engineer</option>
+      </select>
+      <button>submit</button>
+    </form>
+    </div>
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SimpleForm />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
